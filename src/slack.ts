33934bc1eae4b71a3138ab28f771f818130056c9
FarhanMobashir <farhanmobashir007@gmail.com>
2025-01-13 02:41:08 +0530

    feat: basic setup working

--- src/slack.ts
+++ src/slack.ts
@@ -1,14 +1,51 @@
-import type { Handler } from '@netlify/functions';
+import type { Handler } from "@netlify/functions";
+import { slackApi, verifySlackRequests } from "./util/slack";
+import { parse } from "querystring";
 
-export const handler: Handler = async () => {
-	// TODO validate the Slack request
+async function handleSlashCommand(payload: SlackSlashCommandPayload) {
+  switch (payload.command) {
+    case "/foodfight":
+      const response = await slackApi("chat.postMessage", {
+        channel: payload.channel_id,
+        text: "Things are happening!",
+      });
 
-	// TODO handle slash commands
+      if (!response.ok) {
+        console.log(response);
+      }
+      return {
+        statusCode: 20,
+        body: "",
+      };
+    default:
+      return {
+        statusCode: 200,
+        body: `Command ${payload.command} not recognised`,
+      };
+  }
+}
 
-	// TODO handle interactivity (e.g. context commands, modals)
+export const handler: Handler = async (event) => {
+  // TODO validate the Slack request
+  const valid = verifySlackRequests(event);
 
-	return {
-		statusCode: 200,
-		body: 'TODO: handle Slack commands and interactivity',
-	};
+  if (!valid) {
+    console.error("invalid request");
+    return {
+      statusCode: 400,
+      body: "invalid request",
+    };
+  }
+
+  const body = parse(event.body ?? "") as SlackPayload;
+  if (body.command) {
+    return handleSlashCommand(body as SlackSlashCommandPayload);
+  }
+
+  // TODO handle interactivity (e.g. context commands, modals)
+
+  return {
+    statusCode: 200,
+    body: "TODO: handle Slack commands and interactivity , lala",
+  };
 };
