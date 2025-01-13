205d6eae22d69ad05d579696c3417ee9697c19ae
FarhanMobashir <farhanmobashir007@gmail.com>
2025-01-13 10:46:52 +0530

    adds block helpers for block creation

--- src/util/slack.ts
+++ src/util/slack.ts
@@ -32,3 +32,86 @@ export function verifySlackRequests(request: HandlerEvent) {
 
   return `v0=${hash}` === signature;
 }
+
+export const blocks = {
+  section: ({ text }: SectionBlockArgs): SlackBlockSection => {
+    return {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text,
+      },
+    };
+  },
+  input: ({
+    id,
+    placeholder,
+    label,
+    initial_value = "",
+    hint = "",
+  }: InputBlockArgs): SlackBlockInput => {
+    return {
+      block_id: `${id}_block`,
+      type: "input",
+      label: {
+        type: "plain_text",
+        text: label,
+      },
+      element: {
+        action_id: id,
+        type: "plain_text_input",
+        placeholder: {
+          type: "plain_text",
+          text: placeholder,
+        },
+        initial_value,
+      },
+      hint: {
+        type: "plain_text",
+        text: hint,
+      },
+    };
+  },
+  select: ({
+    id,
+    label,
+    placeholder,
+    options,
+  }: SelectBlockArgs): SlackBlockInput => {
+    return {
+      block_id: `${id}_block`,
+      type: "input",
+      label: {
+        type: "plain_text",
+        text: label,
+      },
+      element: {
+        action_id: id,
+        type: "statuc_select",
+        placeholder: {
+          type: "plain_text",
+          text: placeholder,
+          emoji: true,
+        },
+        options: options.map(({ label, value }) => {
+          return {
+            text: {
+              type: "plain_text",
+              text: label,
+              emoji: true,
+            },
+            value,
+          };
+        }),
+      },
+    };
+  },
+};
+
+export function modal({
+  trigger_id,
+  id,
+  title,
+  submit_text = "Submit",
+  blocks,
+}: ModalArgs) {}
